630052789b14f4dd2d51aece8e94e4c0a9299158
João Camacho <j.camacho@campus.fct.unl.pt>
2019-09-29 21:40:49 +0100

    first function

--- Project 1_TP1/Exe1.R
+++ Project 1_TP1/Exe1.R
@@ -1,2 +1,15 @@
 #Exercicio A
-...
+box_muller<-function(n){
+  x<-vector()
+  y<-vector()
+  for (i in 1:n){
+  ome=2*pi*runif(1,0,1)
+  R=sqrt(-2*log(runif(1,0,1)))
+  x=c(x,R*cos(ome))
+  y=c(y,R*sin(ome))
+  }
+  return(list(x,y))
+}
+
+l=box_muller(10)  #list with the first element x and the second y
+
