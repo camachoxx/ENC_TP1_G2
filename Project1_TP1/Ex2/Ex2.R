ab1308a422abc6a145474d2d4f39bbc31e63ac15
João Camacho <j.camacho@campus.fct.unl.pt>
2019-10-03 23:00:05 +0100

    Ex2 falta o plot

--- Project1_TP1/Ex2/Ex2.R
+++ Project1_TP1/Ex2/Ex2.R
@@ -20,14 +20,14 @@ sim.norm<-function(n, mu, std){
   # convert all observations from N(0,1) to N(mu,std)
   x=x*std+mu
   y=y*std+mu
-  return(list(x,y))
+  return(list("x"=x,"y"=y))
 }
 
 sim.quisquared<-function(n,df,lower=0,upper=1){
   #forms a quisquared distribution from random uniform values x=(0 to 1)
   s=vector()
   for (i in 1:n){
-    x=unlist(sim.norm(df,lower,upper)[1])
+    x=sim.norm(df,lower,upper)$x
     s=c(s,sum(x^2))
   }
   return(s)
@@ -46,20 +46,44 @@ box(lwd=2)
 f<-function(x){
   return(x*exp(-x))
 }
-
-M=optimise(h,interval = c(0,5),maximum = T)$objective
-
-gx=sim.quisquared(10000,3)
-
-
-#testing zone
-
 h<-function(x){
   #function h(x)=f(x)/g(x)
-  #this is for testing we can´t use dchisq
   f(x)/dchisq(x,3)
 }
+#the max of h(x)=M
+M=optimise(h,interval = c(0,5),maximum = T)$objective
 
+sim=function(n){
+  v=vector()
+  count=0
+  for (i in 1:n){
+    u=1
+    alpha=0
+    while(u>alpha){
+    x<-sim.quisquared(1,3)
+    alpha=(1/M)*f(x)/dchisq(x,3)
+    u<-runif(1,0,1)
+    count=count+1
+    }
+  v=c(v,x)
+  }
+rr=count/n
+return(list("vector"=v,"rejection_rate"=rr))
+}
+
+set.seed(123)
+fl=sim(10000)
+ff=fl$vector
+rejection_rate=fl$rejection_rate
+# plotting the histogram
+hist(ff,freq=F,main="f(x)",ylim=c(0,0.4),xlab="x",col="grey",
+     cex.main=1.5,cex.lab=1.15)
+# adding the p.d.f. on top of the histogram
+curve(f(x),add=T,lwd=3,lty=1)
+# drawing a box around the plot
+box(lwd=2)
+
+#testing zone
 curve(h(x))
 curve(M*dchisq(x,3),ylim=c(0,0.5),xlim=c(0,10))
 curve(f,type='l',add=T)
